79ba1a2803f1a6d56f92f72b7d0d58682839086c
Ivan Ramos <ivanramos.geek@gmail.com>
2025-01-06 08:55:45 +0800

    add manage-coaches feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/auth/Login";
 import Profile from "./pages/profile/Profile";
 import { Students } from "./pages/students/Students";
 import { Coaches } from "./pages/coaches/Coaches";
+import { Assignment } from "./pages/assignment/Assignment";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/profile" element={<Profile />} />
           <Route path="/students" element={<Students />} />
           <Route path="/coaches" element={<Coaches />} />
+          <Route path="/manage-coaching" element={<Assignment />} />
         </Routes>
       </div>
     </Router>
